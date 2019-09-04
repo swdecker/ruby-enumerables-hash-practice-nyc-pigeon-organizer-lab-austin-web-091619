4566b571f177d1177cf1da84a08772f8bc66ec43
Steven Decker <swdecker@gmail.com>
2019-09-04 23:09:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_hash = {}
+  attribute_array = []
+  data.each_key do |attribute_key|
+    #attribute_key will be :color, :gender, or :lives
+    attribute_array.push(attribute_key)
+    
+    data[attribute_key].each_key do |subattribute_key|
+      
+      
+      data[attribute_key][subattribute_key].each do |name|
+        
+        if !organized_hash[name]
+          organized_hash[name]={}
+          organized_hash[name][attribute_key]=[subattribute_key.to_s]
+        elsif !organized_hash[name][attribute_key]
+          
+          organized_hash[name][attribute_key]=[subattribute_key.to_s]
+        else
+          organized_hash[name][attribute_key].push(subattribute_key.to_s)
+        end
+          
+        
+      end
+      
+      
+    end
+    
+  end
+  
+  organized_hash
 end
