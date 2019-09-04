fdcec493919805314e5a51daa51c8450df6b308c
Steven Decker <swdecker@gmail.com>
2019-09-04 22:51:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
       
       data[attribute_key][subattribute_key].each do |name|
         
-        if organized_hash[name]
+        if organized_hash[name] && organized_hash[name][attribute_key]
           #binding.pry
           organized_hash[name][attribute_key].push(subattribute_key)
       
